052fb082350510126e383df3f6b01af241bcfe4e
Michael Welsh <michaelreedwelsh@gmail.com>
2016-05-18 12:10:23 -0700

    added email confirm check and disabled the button if the two didnt match

--- js/app.js
+++ js/app.js
@@ -2,17 +2,58 @@
 var $all = $("body");
 $("form span").hide();
 
+var $name = $("#name");
+var $mail = $("#mail");
+var $mailConfirm = $("#mail_confirm");
+var $button = $(".btn");
+
+//Abstracting the functions. Modular stuff... K. Whatever....
+function nameLong () {
+	return $name.val().length > 0;
+}
+
+function mailMatch() {
+	return $mailConfirm.val().toLowerCase() === $mail.val().toLowerCase();
+}
+
+//combining both functions to determine whether the button can be used...
+function testBoth () {
+	return mailMatch() && nameLong();
+}
+
+
+function canButton() {
+	$("#butt").prop('disabled', !testBoth());
+}
+
+
+//Hiding the taggy dudes depending on the length of input...
+
 
 function testLength() {
-	if ($(this).val().length > 8) {
+	if (nameLong()) {
 		$(this).next().hide();
 	} else {
 		$(this).next().show();
 	}
 }
 
-//Hiding the taggy dudes depending on the length of input...
+//Function for determining whether email matches confirmation.
 
-$("#name").focus(testLength).keyup(testLength);
+function emailConfirm () {
+	if (mailMatch()) {
+		$(".match").hide();
+	} else {
+		$(".match").show();
+		$button.val("disabled");
+	}
+}
+
+canButton();
+
+
+$name.focus(testLength).keyup(testLength).keyup(canButton);
 	
-$("#mail").focus(testLength).keyup(testLength);
+$mail.focus(emailConfirm).keyup(emailConfirm).keyup(canButton);
+
+$mailConfirm.focus(emailConfirm).keyup(emailConfirm).keyup(canButton);
